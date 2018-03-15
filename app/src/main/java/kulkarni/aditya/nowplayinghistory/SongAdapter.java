4237827addd73ed8de15fc66db2dbd2559748ac0
adikul30 <adi.kul30@gmail.com>
2018-03-15 20:54:30 +0530

    Theme changed | Added click intent

--- app/src/main/java/kulkarni/aditya/nowplayinghistory/SongAdapter.java
+++ app/src/main/java/kulkarni/aditya/nowplayinghistory/SongAdapter.java
@@ -1,14 +1,21 @@
 package kulkarni.aditya.nowplayinghistory;
 
+import android.app.SearchManager;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
@@ -20,6 +27,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by maverick on 3/11/18.
@@ -28,10 +36,12 @@ import java.util.List;
 public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
 
     private List<String> songList;
+    private List<String> songListCopy;
     private Context mContext;
 
     public SongAdapter(List<String> songList, Context mContext) {
         this.songList = songList;
+        this.songListCopy = songList;
         this.mContext = mContext;
     }
 
@@ -55,13 +65,27 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView title;
-        LinearLayout songLayout;
+        ImageView copyText;
+        RelativeLayout songLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.song_title);
+            copyText = itemView.findViewById(R.id.clipboard_action);
             songLayout = itemView.findViewById(R.id.song_layout);
             songLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String songTitle = songList.get(getAdapterPosition());
+                    Intent intent = new Intent(Intent.ACTION_SEARCH);
+                    intent.putExtra(SearchManager.QUERY, songTitle);
+                    if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+                        mContext.startActivity(intent);
+                    }
+                }
+            });
+
+            copyText.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     String songTitle = songList.get(getAdapterPosition());
@@ -72,5 +96,19 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
                 }
             });
         }
+    }
+
+    public void filter(String searchQuery){
+        searchQuery = searchQuery.toLowerCase(Locale.getDefault());
+        songList.clear();
+        if(searchQuery.length() == 0){
+            songList.addAll(songListCopy);
+        }
+        else {
+            for (String item : songListCopy) {
+                if(item.contains(searchQuery))songList.add(item);
+            }
+        }
+        notifyDataSetChanged();
     }
 }
